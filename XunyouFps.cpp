13e9c73d85e6a816a483f270cfddac8f4239ac44
unknown <1939515407@qq.com>
2024-09-25 15:59:31 +0800

    迅游帧率优化项目

---
+++ XunyouFps.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+#include <Windows.h>
+
+
+
+
+
+int main()
+{
+    HKEY hKey;
+    //      鼠标加速
+    RegCreateKeyExA(HKEY_CURRENT_USER, "Control Panel\\Mouse", 0, 0, 0, KEY_ALL_ACCESS, 0, &hKey, 0);
+    //RegCreateKeyExA(HKEY_CURRENT_USER, "Control Panel\\Mouse", 0, 0, 0, 0x00020106, 0, &hKey, 0);
+}
